aa8a8f96a61fe89d387d90d73a1fdea8763270ef
Beaujean Cyril <cy.beaujean@gmail.com>
2026-04-06 11:11:13 +0200

    fix: ESM compat for seed script (__dirname)

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,10 @@
 import { PrismaClient } from '@prisma/client'
 import { readFileSync } from 'fs'
-import { resolve } from 'path'
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const prisma = new PrismaClient()
 
